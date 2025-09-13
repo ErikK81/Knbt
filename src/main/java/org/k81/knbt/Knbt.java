4c3a8dac2fd99914583233f7bfb97eed17e13de5
ErikK81 <erikcriscuolo81@gmail.com>
2025-09-12 23:09:39 -0300

    Criando repositório

---
+++ src/main/java/org/k81/knbt/Knbt.java
@@ -0,0 +1,17 @@
+package org.k81.knbt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Knbt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Pluin shutdown logic
+    }
+}
