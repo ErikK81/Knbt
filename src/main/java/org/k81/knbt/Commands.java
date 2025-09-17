4b72f3de52b6c73552cb020fa294814f06a96731
ErikK81 <erikcriscuolo81@gmail.com>
2025-09-17 19:55:03 -0300

    Funções primárias de list e get adicionadas

--- src/main/java/org/k81/knbt/Commands.java
+++ src/main/java/org/k81/knbt/Commands.java
@@ -13,7 +13,7 @@ public class Commands implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
 
         if (!(sender instanceof Player player)) {
-            Bukkit.getConsoleSender().sendMessage("Console cannot send this command!");
+            Bukkit.getConsoleSender().sendMessage("Console cant execute this command!");
             return true;
         }
 
@@ -28,64 +28,26 @@ public class Commands implements CommandExecutor {
 
         switch (args[0].toLowerCase()) {
             case "set":
-                if (args.length >= 3) {
-                    // Junta args[2...] no caso de o valor ter espaço
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 2; i < args.length; i++) {
-                        if (i > 2) sb.append(" ");
-                        sb.append(args[i]);
-                    }
-                    Utils.SetNbt(player, args[1], sb.toString());
-                    player.sendMessage("§aTag '" + args[1] + "' definida para '" + sb + "'.");
-                } else {
-                    player.sendMessage("§cUse: /knbt set <tag> <valor>");
+                if (args.length == 3){
+                    Utils.SetTag(player, args[1], args[2]);
                 }
                 break;
 
             case "get":
-                if (args.length == 2) {
-                    Object value = Utils.GetNbt(player, args[1]);
-                    if (value != null) {
-                        player.sendMessage("§aTag '" + args[1] + "' = " + value);
-                    } else {
-                        player.sendMessage("§eTag '" + args[1] + "' não encontrada.");
-                    }
-                } else {
-                    player.sendMessage("§cUse: /knbt get <tag>");
-                }
+                    Utils.GetTag(player, args[1]);
                 break;
 
             case "remove":
-                if (args.length == 2) {
-                    Utils.RemoveNbt(player, args[1]);
-                    player.sendMessage("§aTag '" + args[1] + "' removida.");
-                } else {
-                    player.sendMessage("§cUse: /knbt remove <tag>");
-                }
+                    // Utils.RemoveNbt(player, args[1]);
                 break;
 
             case "list":
-                if (args.length == 1) {
-                    var keys = Utils.ListNbt(player);
-                    if (keys.isEmpty()) {
-                        player.sendMessage("§eNenhuma tag encontrada neste item.");
-                    } else {
-                        player.sendMessage("§aTags do item:");
-                        for (String key : keys) {
-                            Object value = Utils.GetNbt(player, key);
-                            player.sendMessage(" - " + key + " = " + value);
-                        }
-                    }
-                } else {
-                    player.sendMessage("§cUse: /knbt list");
-                }
-                break;
-
+                    Utils.listTag(player); // Map<String, Object>
+                    break;
             default:
-                player.sendMessage("§cComando inválido! Use: /knbt <set|get|remove|list>");
+                player.sendMessage("§cInvalid command! Use: /knbt <set|get|remove|list>");
                 break;
         }
-
-        return true;
+    return true;
     }
 }
--- src/main/java/org/k81/knbt/Utils.java
+++ src/main/java/org/k81/knbt/Utils.java
@@ -1,67 +1,68 @@
 package org.k81.knbt;
 
-import de.tr7zw.nbtapi.NBTItem;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Set;
+import java.util.*;
 
 public class Utils {
 
-    // Define NBT de acordo com o tipo do valor
-    public static void SetNbt(Player p, String tagName, Object value) {
+    public static void SetTag(Player p, String key, String value) {
         ItemStack item = p.getInventory().getItemInMainHand();
         if (item.getType().isAir()) return;
 
-        NBTItem nbt = new NBTItem(item);
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
 
-        switch (value) {
-            case String str -> nbt.setString(tagName, str);
-            case Integer i -> nbt.setInteger(tagName, i);
-            case Double d -> nbt.setDouble(tagName, d);
-            case Boolean b -> nbt.setBoolean(tagName, b);
-            default -> throw new IllegalArgumentException("Tipo não suportado: " + value.getClass());
+        meta.setItemModel(NamespacedKey.minecraft(key));
+
+        item.setItemMeta(meta);
+        p.sendMessage("§6--- Value " + value + " set on " + key + "---");
+    }
+
+
+    public static void GetTag(Player p, String key) {
+        ItemStack item = p.getInventory().getItemInMainHand();
+        ItemMeta meta = item.getItemMeta();
+        String raw = meta.serialize().get(key).toString();
+        p.sendMessage("§6--- Tag Information ---");
+        p.sendMessage("§b" + key + "§7 = §f" + raw);
+    }
+
+    public static void listTag(Player p) {
+        ItemStack item = p.getInventory().getItemInMainHand();
+        ItemMeta meta = item.getItemMeta();
+
+        if (item.getType().isAir()) {
+            p.sendMessage("§eYou aren't holding an item!.");
+            return;
+        }
+        if (meta == null) {
+            p.sendMessage("§eThis item hasn't meta.");
+            return;
         }
 
-        p.getInventory().setItemInMainHand(nbt.getItem());
-    }
+        p.sendMessage("§6--- Item Information ---");
+        p.sendMessage("Material: §f" + item.getType());
+        p.sendMessage("Amount: §f" + item.getAmount());
+        p.sendMessage("CustomModelData: §f" + (meta.hasCustomModelData() ? meta.getCustomModelData() : "None"));
 
-    // Recupera um NBT
-    public static Object GetNbt(Player p, String tagName) {
-        ItemStack item = p.getInventory().getItemInMainHand();
-        if (item.getType().isAir()) return null;
-
-        NBTItem nbt = new NBTItem(item);
-        if (!nbt.hasKey(tagName)) return null;
-
-        try { return nbt.getString(tagName); } catch (Exception ignored) {}
-        try { return nbt.getInteger(tagName); } catch (Exception ignored) {}
-        try { return nbt.getDouble(tagName); } catch (Exception ignored) {}
-        try { return nbt.getBoolean(tagName); } catch (Exception ignored) {}
-
-        return null;
-    }
-
-    // Remove uma tag NBT do item
-    public static void RemoveNbt(Player p, String tagName) {
-        ItemStack item = p.getInventory().getItemInMainHand();
-        if (item.getType().isAir()) return;
-
-        NBTItem nbt = new NBTItem(item);
-
-        if (nbt.hasKey(tagName)) {
-            nbt.removeKey(tagName);
-            p.getInventory().setItemInMainHand(nbt.getItem());
+        if (meta.hasLore()) {
+            p.sendMessage("Lore:");
+            Objects.requireNonNull(meta.lore()).forEach(line -> p.sendMessage("§7- " + line));
+        } else {
+            p.sendMessage("Lore: None");
         }
-    }
 
-    // Lista todas as tags NBT do item
-    public static Set<String> ListNbt(Player p) {
-        ItemStack item = p.getInventory().getItemInMainHand();
-        if (item.getType().isAir()) return Set.of();
+        p.sendMessage("§6--- ItemMeta ---");
+        for (Map.Entry<String, Object> raw : meta.serialize().entrySet()){
 
-        NBTItem nbt = new NBTItem(item);
-
-        return nbt.getKeys(); // Retorna todas as chaves em um Set<String>
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            p.sendMessage("§b" + gson.toJson(raw.getKey()) + "§7 = §f" + gson.toJson(raw.getValue()));
+        }
     }
 }
